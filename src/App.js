bdea4332157d039d87914929f8b6059f04a496db
anshumantsagar <anshumantsagar@gmail.com>
2020-05-03 18:33:42 +0530

    styling react component & elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
+// import Radium,{StyleRoot} from 'radium'
+// import styled from 'styled-components'
 import Person from './components/Person'
-
-
 
 class App extends Component{
   state = {
@@ -39,11 +39,16 @@ class App extends Component{
 
   render(){
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'point'
+      cursor: 'point',
+      ':hover':{
+        backgroundColor : 'lightgreen',
+        color:'black'
+      }
     }
 
     let persons = null
@@ -56,15 +61,28 @@ class App extends Component{
           })}
         </div>
       )
+    style.backgroundColor='red'
+    style[':hover']={
+      backgroundColor : 'salmon',
+      color:'black'
+    }
+    }
+
+    let classes = []
+    if(this.state.persons.length <=2){
+      classes.push('red')
+    }
+    if(this.state.persons.length <=1){
+      classes.push('bold')
     }
 
     return (
-      <div className="App">
-        <h1>I'm a react App</h1>
-        <p>This is really working</p>
-        <button style={style} onClick={this.togglePersonHandler}>Toggle Person</button>
-        {persons}
-      </div>
+        <div className="App">
+          <h1>I'm a react App</h1>
+          <p className={classes.join(' ')}>This is really working</p>
+          <button className='button' onClick={this.togglePersonHandler}>Toggle Person</button>
+          {persons}
+        </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'I\'m a react App'))
   }
